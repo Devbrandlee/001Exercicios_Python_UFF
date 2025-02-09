62922e8f621932ca1b01358067384a34a7a7d6a0
Devbrandlee <l33bran@gmail.com>
2025-02-09 18:42:22 -0300

    First commit

---
+++ modulo_1/matematica_Basica.py
@@ -0,0 +1,36 @@
+#a = 10
+#b = 15
+
+#x = a + b 
+
+
+#print (x)
+
+
+#usuário digitando
+
+ax = int (input())
+bx = int (input())
+
+c= ax + bx
+
+print (type (c))
+print (c)
+
+#Outras formas
+
+a = int(input(" Digite o 1 valor "))
+
+b = int(input (" digite o 2 valor "))
+
+print(f"O valor da  some é : {a + b}") 
+
+
+#float 
+
+d = float(input())
+
+f = float (input())
+
+x = d + f
+print(f"A soma entre o primeiro valor e o segundo é : ", x)
